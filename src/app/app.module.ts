3ae533ae261872173c3ec649319cc9c78e918742
aliyalama@googlemail.com <Gundam1979!>
2021-06-20 01:00:11 +0200

    wip first steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EncryptionComponent } from './encryption/encryption.component';
-
+import {MDBBootstrapModule} from "angular-bootstrap-md";
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import {RouterModule} from "@angular/router";
 @NgModule({
   declarations: [
     AppComponent,
-    EncryptionComponent
+    EncryptionComponent,
+    NavbarComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing-module.ts
+++ src/app/app.routing-module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {EncryptionComponent} from "./encryption/encryption.component";
+import {AppComponent} from "./app.component";
+import {LandingComponent} from "./landing/landing.component";
 
 const routes : Routes = [
-  {path: 'encrypt', component: EncryptionComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'encrypt', component: EncryptionComponent},
+  {path: 'home', component: LandingComponent}
 ];
 
 @NgModule({
