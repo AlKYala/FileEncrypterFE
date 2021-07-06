71fbed8512da8ce004ef2bb3fa246768220619fc
aliyalama@googlemail.com <Gundam1979!>
2021-07-06 18:42:45 +0200

    encryption works decryption upload wip

--- src/shared/Base64File/model/ExtendedBase64File.ts
+++ src/shared/Base64File/model/ExtendedBase64File.ts
@@ -3,9 +3,23 @@ import {Base64File} from "./Base64File";
 export class ExtendedBase64File {
   public content: Base64File;
   public key: Base64File;
+  public parent: Base64File;
 
-  public constructor(content: Base64File, key: Base64File) {
-    this.content = content;
-    this.key = key;
+  public constructor() {
+    this.content = new Base64File("", "", "");
+    this.key = new Base64File("", "", "");
+    this.parent = new Base64File("", "", "");
+  }
+
+  public setContent(file: Base64File): void {
+    this.content = file;
+  }
+
+  public setKey(file: Base64File): void {
+    this.key = file;
+  }
+
+  public setParent(file: Base64File): void {
+    this.parent = file;
   }
 }
